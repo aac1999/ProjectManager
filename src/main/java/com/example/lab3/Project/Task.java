34ea33656e581a6122080da46fbfd1281ee2db13
Adil Chohan <adilchohan1999@gmail.com>
2023-10-04 11:26:59 +0200

    small ändring

--- src/main/java/com/example/lab3/Project/Task.java
+++ src/main/java/com/example/lab3/Project/Task.java
@@ -2,11 +2,12 @@ package com.example.lab3.Project;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Task implements Comparable<Task>, Serializable { //Tror det ska vara Comparable<Task>?
 
-    private String descriptions;
-    private int id;
+    private final String descriptions;
+    private final int id;
     private String Takenby;
     private TaskState state;
     private LocalDate lastUpdate;
@@ -20,6 +21,30 @@ public class Task implements Comparable<Task>, Serializable { //Tror det ska var
         this.state = TaskState.TO_DO;
         this.lastUpdate = LocalDate.now();
         this.prio = prio;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTakenby() {
+        return Takenby;
+    }
+
+    public TaskState getState() {
+        return state;
+    }
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public TaskPrio getPrio() {
+        return prio;
     }
 
     public void setTakenby(String takenby) {
@@ -46,5 +71,31 @@ public class Task implements Comparable<Task>, Serializable { //Tror det ska var
             result = this.descriptions.compareTo(other.descriptions); //fråga Anders OM RÄTT
         }
         return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if(other instanceof Task) {
+            Task otherTask = (Task) other; // downcast reference type to Task
+            return this.compareTo(otherTask) == 0;
+            //return this.prio == otherTask.prio && this.descriptions == otherTask.descriptions;
+        }
+        return false;
+        /*
+        if (!(other instanceof Task task)) return false;
+        return Objects.equals(descriptions, task.descriptions) && prio == task.prio; */
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "descriptions='" + descriptions + '\'' +
+                ", id=" + id +
+                ", Takenby='" + Takenby + '\'' +
+                ", state=" + state +
+                ", lastUpdate=" + lastUpdate +
+                ", prio=" + prio +
+                '}';
     }
 }
